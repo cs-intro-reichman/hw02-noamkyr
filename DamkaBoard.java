811a7cf9449ad22b836d01a820ed16184819cd94
noam <noam.kyram@post.runi.ac.il>
2023-12-27 21:51:11 +0200

    fixed space at end

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -16,13 +16,7 @@ public class DamkaBoard {
             }
             // init a loop that runs n time that prints a line
             for (int j = 0; j < n; j++) {
-
-                // dont unnecessary space at the end
-                if (j == n - 1){
-                    System.out.print("*");
-                } else {
-                    System.out.print("* ");
-                }
+                System.out.print("* ");
             }
             // change the boolean to the opposite value
             space_at_start = !space_at_start;
