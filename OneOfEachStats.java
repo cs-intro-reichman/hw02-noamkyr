061cf18e3d53e2c2d69077313477623192bca79a
noam <noam.kyram@post.runi.ac.il>
2023-12-27 20:54:58 +0200

    added question 8

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -10,10 +10,10 @@ import java.util.Random;
 public class OneOfEachStats {
 	public static void main (String[] args) {
 		// Gets the two command-line arguments
-		int T = Integer.parseInt(args[0]);
+		// int T = Integer.parseInt(args[0]);
 		int seed = Integer.parseInt(args[1]);
 		// Initailizes a random numbers generator with the given seed value
-        Random generator = new Random(seed);  
+		Random generator = new Random(seed);
 		
 		//// In the previous version of this program, you used a statement like:
 		//// double rnd = Math.random();
@@ -24,6 +24,91 @@ public class OneOfEachStats {
 		//// just like you had in the previous version, except that the 
 		//// randomization will be based on the given seed.
 		//// This is the only change that you have to do in the program.
-		    
+
+		// define t as parameter from args
+		int t = Integer.parseInt(args[0]);
+		// init the value of sum of born children (to calculate the average)
+		int sum_children = 0;
+		// init the variable that counts number of families with 2 children
+		int two_children = 0;
+		// init the variable that counts number of families with 3 children
+		int three_children = 0;
+		// init the variable that counts number of families with 4 or more children
+		int four_or_more = 0;
+		for (int i = 0; i < t; i++) {
+			// init the boolean variable that indicates if a boy was born with false (a boy wasnt born yet)
+			boolean have_boy = false;
+			// init the boolean variable that indicates if a girl was born with false (a girl wasnt born yet)
+			boolean have_girl = false;
+			// init the variable the count of children in each family
+			int count = 0;
+			// do the commands inside the while if a family have at least one boy and girl yet
+			while (have_boy == false || have_girl == false){
+				// init variable that generates random number - 0 or 1
+				int x = (int) (generator.nextDouble() * 2);
+
+				// 0 if a girl was born
+				if (x == 0){
+					// check if a girl was born yet
+					if (have_girl == false){
+						// change the value because a girl was just born
+						have_girl = true;
+					}
+					// if a boy was born
+				} else {
+					// check if a boy was born yet
+					if (have_boy == false){
+						// change the value because a boy was just born
+						have_boy = true;
+					}
+				}
+				// increase the count of born children
+				count ++;
+			}
+			// add the sum of the count of children of this current family
+			sum_children += count;
+			// check the value of count
+			if (count == 2){
+				// increase the value of number of families with 2 children
+				two_children ++;
+			} else {
+				if (count == 3){
+					// increase the value of number of families with 3 children
+					three_children ++;
+				} else {
+					// increase the value of number of families with 4 or more children
+					four_or_more ++;
+				}
+			}
+
+		}
+		// calculating the value average children per family and casting the result to double
+		double avg = (double)(sum_children)/t;
+		// printing the results
+		System.out.println("Average: " + avg + " children to get at least one of each gender.");
+		System.out.println("Number of families with 2 children: " + two_children);
+		System.out.println("Number of families with 3 children: " + three_children);
+		System.out.println("Number of families with 4 or more children: " + three_children);
+
+		// get the maximum value of the 3 variables using math.max function
+		int most_common = Math.max(Math.max(two_children, three_children), four_or_more);
+
+		// check if the most common case is family with 2 children
+		if (most_common == two_children){
+			// print result
+			System.out.println("The most common number of children is 2.");
+		} else {
+			// check if the most common case is family with 3 children
+			if (most_common == three_children){
+				// print result
+				System.out.println("The most common number of children is 3.");
+			} else {
+				// check if the most common case is family with 4 or more children - default case
+				// print result
+				System.out.println("The most common number of children is 4 or more.");
+			}
+		}
+
+
 	}
 }
